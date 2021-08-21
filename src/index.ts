92ce18ed8d7d3879c58dde9745f62729a7120e7e
kazuhi-ra <kazuhira4126@gmail.com>
2021-08-21 22:02:01 +0900

    ボタンを本体に組み込む

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 import { css, html, LitElement } from 'lit'
-import { customElement } from 'lit/decorators.js'
+import { customElement, state } from 'lit/decorators.js'
 
+import { Part } from './components/stop-button'
 import { normalizeCss } from './utils/normalizeCss'
+
+import './components/start-button'
+import './components/stop-button'
 
 @customElement('ashitano-vest')
 export class AshitanoVest extends LitElement {
+  @state()
+  i = 0
+
+  @state()
+  _isHeadSpinning = false
+
+  @state()
+  _isVestSpinning = false
+
+  @state()
+  _isPantsSpinning = false
+
   static get styles() {
     return [
       normalizeCss,
@@ -16,10 +32,58 @@ export class AshitanoVest extends LitElement {
     ]
   }
 
+  private onStart = () => {
+    this._isHeadSpinning = true
+    this._isVestSpinning = true
+    this._isPantsSpinning = true
+  }
+
+  private onStop = (event: CustomEvent<Part>) => {
+    const part = event.detail
+
+    switch (part) {
+      case 'head':
+        this._isHeadSpinning = false
+        break
+      case 'vest':
+        this._isVestSpinning = false
+        break
+      case 'pants':
+        this._isPantsSpinning = false
+        break
+    }
+
+    if (this.isAllStopped()) {
+      this.stopAll()
+    }
+  }
+
+  private isAllStopped = () =>
+    !(this._isHeadSpinning || this._isVestSpinning || this._isPantsSpinning)
+
+  private stopAll = (): void => {
+    this._isHeadSpinning = false
+    this._isVestSpinning = false
+    this._isPantsSpinning = false
+  }
+
   render() {
     return html`
-      <div>
+      <div @start=${this.onStart} @stop=${this.onStop}>
         <div>yo</div>
+        <start-button .disabled=${!this.isAllStopped()}></start-button>
+        <stop-button
+          .disabled=${!this._isHeadSpinning}
+          part="head"
+        ></stop-button>
+        <stop-button
+          .disabled=${!this._isVestSpinning}
+          part="vest"
+        ></stop-button>
+        <stop-button
+          .disabled=${!this._isPantsSpinning}
+          part="pants"
+        ></stop-button>
       </div>
     `
   }
