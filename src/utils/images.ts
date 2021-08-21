4815185b214d06e584c60cb3e27655b8b805253f
kazuhi-ra <kazuhira4126@gmail.com>
2021-08-21 22:00:48 +0900

    ルーレットに使う画像の用意をする

---
+++ src/utils/images.ts
@@ -0,0 +1,27 @@
+export const heads: string[] = [
+  'https://next-vest.kazuhira.dev/images/h1.jpg',
+  'https://next-vest.kazuhira.dev/images/h2.jpg',
+  'https://next-vest.kazuhira.dev/images/h3.jpg',
+]
+
+export const vests = [
+  'https://next-vest.kazuhira.dev/images/v1.jpg',
+  'https://next-vest.kazuhira.dev/images/v2.jpg',
+  'https://next-vest.kazuhira.dev/images/v3.jpg',
+  'https://next-vest.kazuhira.dev/images/v4.jpg',
+  'https://next-vest.kazuhira.dev/images/v5.jpg',
+  'https://next-vest.kazuhira.dev/images/v6.jpg',
+  'https://next-vest.kazuhira.dev/images/v7.jpg',
+  'https://next-vest.kazuhira.dev/images/v8.jpg',
+  'https://next-vest.kazuhira.dev/images/v9.jpg',
+]
+
+export const pants = [
+  'https://next-vest.kazuhira.dev/images/p1.jpg',
+  'https://next-vest.kazuhira.dev/images/p2.jpg',
+  'https://next-vest.kazuhira.dev/images/p3.jpg',
+  'https://next-vest.kazuhira.dev/images/p4.jpg',
+  'https://next-vest.kazuhira.dev/images/p5.jpg',
+]
+
+export const background = 'https://next-vest.kazuhira.dev/images/renga2.png'
